a101cfee99b0025c4a0170ee72bb4391ecf95f5b
Ngan Phan NFQ <ngan.phan@nfq.com>
2023-07-23 16:35:35 +0700

    fix: :bug: [FH-3] Replace migration files

--- src/database/migrations/Migration20230723093403_new_user_table.ts
+++ src/database/migrations/Migration20230723093403_new_user_table.ts
@@ -1,9 +1,9 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20230723084734_new_user_table extends Migration {
+export class Migration20230723093403_new_user_table extends Migration {
   async up(): Promise<void> {
     this.addSql(
-      'create table `user` (`id` int unsigned not null auto_increment primary key, `created_at` datetime not null, `updated_at` datetime not null, `google_id` varchar(255) not null, `email` varchar(255) not null, `name` varchar(255) not null) default character set utf8mb4 engine = InnoDB;',
+      'create table `user` (`id` int unsigned not null auto_increment primary key, `created_at` datetime not null, `updated_at` datetime not null, `google_id` varchar(255) not null, `email` varchar(255) not null, `name` varchar(255) not null, `photo` varchar(255) null) default character set utf8mb4 engine = InnoDB;',
     );
     this.addSql(
       'alter table `user` add unique `user_google_id_unique`(`google_id`);',
