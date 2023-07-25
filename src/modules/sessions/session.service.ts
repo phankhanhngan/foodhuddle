6c219c2a4bc929a7ab730302d0faf4dfe0a00fef
DuyTran <duy.tran@nfq.com>
2023-07-26 06:39:03 +0700

    [FH-6]: feat: adding try catch for function getting sessions today

--- src/modules/sessions/session.service.ts
+++ src/modules/sessions/session.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@mikro-orm/nestjs';
-import { EntityManager, EntityRepository  } from '@mikro-orm/core';
+import { EntityManager, EntityRepository } from '@mikro-orm/core';
 import { Session } from 'src/entities/session.entity';
+import e from 'express';
 
 @Injectable()
 export class SessionService {
@@ -13,31 +14,36 @@ export class SessionService {
 
     async getAllSessionsToday() {
 
-        const today = new Date();
-        const currentDate = today.getDate();
-        const currentMonth = today.getMonth() + 1;
-        const currentYear = today.getFullYear();
+        try {
+            const today = new Date();
+            const currentDate = today.getDate();
+            const currentMonth = today.getMonth() + 1;
+            const currentYear = today.getFullYear();
 
-        const allSessions = this.sessionRepository.findAll({ fields: ['id', 'title', 'host', 'status', 'created_at'] })
+            const allSessions = this.sessionRepository.findAll({ fields: ['id', 'title', 'host', 'status', 'created_at'] })
 
-        const listSessionsToday = (await allSessions).filter((v) =>
-            (v.created_at.getDate() === currentDate) &&
-            ((v.created_at.getMonth() + 1) === currentMonth) &&
-            (v.created_at.getFullYear() === currentYear)
-        );
+            const listSessionsToday = (await allSessions).filter((v) =>
+                (v.created_at.getDate() === currentDate) &&
+                ((v.created_at.getMonth() + 1) === currentMonth) &&
+                (v.created_at.getFullYear() === currentYear)
+            );
 
-        const listSessionsReturn = (await listSessionsToday).map((v) =>{
-            return {
-                id: v.id,
-                title: v.title,
-                host: v.host.email,
-                status: v.status,
-                created_at: v.created_at,
-                number_of_joiners: 0
-            };
-        });
+            const listSessionsReturn = listSessionsToday.map((v) => {
+                return {
+                    id: v.id,
+                    title: v.title,
+                    host: v.host.email,
+                    status: v.status,
+                    created_at: v.created_at,
+                    number_of_joiners: 0
+                };
+            });
 
-        return listSessionsReturn;
+            return listSessionsReturn;
+        } catch (error) {
+            console.log('HAS AN ERRO AT getAllSessionsToday()');
+            throw error;
+        }
     }
 
 }
