7c1c1ab4ca56d09017fd277d5d26dac6b4f7c486
Ngan Phan NFQ <ngan.phan@nfq.com>
2023-07-26 17:35:56 +0700

    style: :art: [FH-8] Beautifuler SQL

--- src/database/migrations/Migration20230726034849_new_food_order_table.ts
+++ src/database/migrations/Migration20230726034849_new_food_order_table.ts
@@ -3,27 +3,55 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20230726034849_new_food_order_table extends Migration {
   async up(): Promise<void> {
     this.addSql(
-      'create table `food_order` (`id` int unsigned not null auto_increment primary key, `created_at` datetime not null, `updated_at` datetime not null, `session_id` int unsigned not null, `user_id` int unsigned not null, `food_name` varchar(255) not null, `origin_price` int not null, `options` varchar(255) null, `quantity` int not null, `note` varchar(255) null, `actual_price` int null) default character set utf8mb4 engine = InnoDB;',
+      `CREATE TABLE food_order (
+        id INT unsigned NOT NULL auto_increment PRIMARY KEY,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL,
+        session_id INT unsigned NOT NULL,
+        user_id INT unsigned NOT NULL,
+        food_name VARCHAR(255) NOT NULL,
+        origin_price INT NOT NULL,
+        options VARCHAR(255) NULL,
+        quantity INT NOT NULL,
+        note VARCHAR(255) NULL,
+        actual_price INT NULL
+        ) DEFAULT CHAR
+      
+      SET utf8mb4 engine = InnoDB;
+      `,
     );
     this.addSql(
-      'alter table `food_order` add index `food_order_session_id_index`(`session_id`);',
+      `ALTER TABLE food_order ADD INDEX food_order_session_id_index (session_id);`,
     );
     this.addSql(
-      'alter table `food_order` add index `food_order_user_id_index`(`user_id`);',
+      `ALTER TABLE food_order ADD INDEX food_order_user_id_index (user_id);`,
     );
     this.addSql(
-      'alter table `food_order` add unique `food_order_session_id_user_id_food_name_unique`(`session_id`, `user_id`, `food_name`);',
+      `ALTER TABLE food_order ADD UNIQUE food_order_session_id_user_id_food_name_unique (
+        session_id,
+        user_id,
+        food_name
+        );
+      `,
     );
 
     this.addSql(
-      'alter table `food_order` add constraint `food_order_session_id_foreign` foreign key (`session_id`) references `session` (`id`) on update cascade;',
+      `ALTER TABLE food_order 
+      ADD CONSTRAINT food_order_session_id_foreign 
+      FOREIGN KEY (session_id) REFERENCES session (id) 
+      ON UPDATE CASCADE;
+      `,
     );
     this.addSql(
-      'alter table `food_order` add constraint `food_order_user_id_foreign` foreign key (`user_id`) references `user` (`id`) on update cascade;',
+      `ALTER TABLE food_order 
+      ADD CONSTRAINT food_order_user_id_foreign 
+      FOREIGN KEY (user_id) REFERENCES user (id)
+      ON UPDATE CASCADE;
+      `,
     );
   }
 
   async down(): Promise<void> {
-    this.addSql('drop table if exists `food_order`;');
+    this.addSql(`DROP TABLE IF EXISTS food_order;`);
   }
 }
