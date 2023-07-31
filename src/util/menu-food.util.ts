e0155dcccc1529e0d609c78e423dfad1290840d0
DuyTran <duy.tran@nfq.com>
2023-07-31 14:02:46 +0700

    [FH-21]: feat: check valid link before craping menu food

--- src/util/menu-food.util.ts
+++ src/util/menu-food.util.ts
@@ -32,59 +32,80 @@ export class MenuShopUtil {
 
       const urlGetShopId = `https://gappapi.deliverynow.vn/api/delivery/get_from_url?url=${getShopUrl}`;
 
-      const shopId = (await axios.get(urlGetShopId, config)).data?.reply
-        .delivery_id;
+      const checkConnection = (await axios.get(urlGetShopId, config)).data
+        .reply;
+
+      if (!checkConnection) {
+        return {
+          status: 400,
+          message: `Invalid shop link !`,
+        };
+      }
+
+      const shopId = checkConnection.delivery_id;
 
       const urlGetShopMenu = `https://gappapi.deliverynow.vn/api/dish/get_delivery_dishes?id_type=2&request_id=${shopId}`;
 
       const reponseData = (await axios.get(urlGetShopMenu, config)).data;
 
-      const menuFoodFormated = reponseData.reply.menu_infos.map((v) => {
-        const foodByDish = v.dishes.map((fbd) => {
-          const isFetchData = fbd.is_active && fbd.is_available;
+      if (reponseData.reply) {
+        const menuFoodFormated = reponseData.reply.menu_infos.map((v) => {
+          const foodByDish = v.dishes.map((fbd) => {
+            const isFetchData = fbd.is_active && fbd.is_available;
 
-          if (isFetchData) {
-            const optionsFood = fbd.options
-              ? fbd.options.map((op) => {
-                  const optionItems = op.option_items.items
-                    ? op.option_items.items.map((opi) => {
-                        const optionItem = {
-                          name: opi.name,
-                          price: opi.price.value,
-                        };
+            if (isFetchData) {
+              const optionsFood = fbd.options
+                ? fbd.options.map((op) => {
+                    const optionItems = op.option_items.items
+                      ? op.option_items.items.map((opi) => {
+                          const optionItem = {
+                            name: opi.name,
+                            price: opi.price.value,
+                          };
 
-                        return optionItem;
-                      })
-                    : [];
+                          return optionItem;
+                        })
+                      : [];
 
-                  const option = {
-                    id: op.id,
-                    mandatory: op.mandatory,
-                    name: op.name,
-                    option_items: optionItems,
-                  };
-                  return option;
-                })
-              : [];
+                    const option = {
+                      id: op.id,
+                      mandatory: op.mandatory,
+                      name: op.name,
+                      option_items: optionItems,
+                    };
+                    return option;
+                  })
+                : [];
 
-            const menuFood = {
-              id: fbd.id,
-              name: fbd.name,
-              description: fbd.description,
-              price: fbd.price.value,
-              discount_price: fbd.discount_price ? fbd.discount_price.value : 0,
-              photo: fbd.photos[0].value,
-              options: optionsFood,
-            };
+              const menuFood = {
+                id: fbd.id,
+                name: fbd.name,
+                description: fbd.description,
+                price: fbd.price.value,
+                discount_price: fbd.discount_price
+                  ? fbd.discount_price.value
+                  : 0,
+                photo: fbd.photos[0].value,
+                options: optionsFood,
+              };
 
-            return menuFood;
-          }
+              return menuFood;
+            }
+          });
+
+          return foodByDish;
         });
 
-        return foodByDish;
-      });
-
-      return menuFoodFormated;
+        return {
+          status: 200,
+          data: menuFoodFormated,
+        };
+      } else {
+        return {
+          status: 400,
+          message: `Invalid shop link !`,
+        };
+      }
     } catch (error) {
       throw error;
     }
