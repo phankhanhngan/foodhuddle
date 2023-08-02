393cc929ca1517761a9eec2cdaa5607c42d5624a
DuyTran <duy.tran@nfq.com>
2023-08-02 10:58:22 +0700

    [FH-10]: feat: edit api name for edit status session

--- src/modules/sessions/session.controller.ts
+++ src/modules/sessions/session.controller.ts
@@ -133,7 +133,7 @@ export class SessionController {
     }
   }
 
-  @Put('/:id')
+  @Put('/:id/update-status')
   @UseGuards(JwtAuthGuard)
   async updateSessionStatus(
     @Body() dto: UpdateSessionStatus,
