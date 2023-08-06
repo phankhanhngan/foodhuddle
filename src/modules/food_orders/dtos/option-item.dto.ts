4388c8fdf001181bdee1aeef8fc785c33f559d0e
DuyTran <duy.tran@nfq.com>
2023-08-07 02:20:52 +0700

    feat: removing maxQuantity

--- src/modules/food_orders/dtos/option-item.dto.ts
+++ src/modules/food_orders/dtos/option-item.dto.ts
@@ -4,9 +4,6 @@ export class OptionItemDTO {
   @IsString({ message: 'Option name must be a string' })
   name: string;
 
-  @IsInt({ message: 'Max quantity must be an integer' })
-  maxQuantity: number;
-
   @IsInt({ message: 'Option price must be an integer' })
   price: number;
 }
