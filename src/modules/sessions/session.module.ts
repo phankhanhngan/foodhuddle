86a22ba628e76c2b621716fe60fbbfc0b8840668
DuyTran <duy.tran@nfq.com>
2023-08-07 07:43:14 +0700

    [FH-7]: chore: removing ImageResize in session.module

--- src/modules/sessions/session.module.ts
+++ src/modules/sessions/session.module.ts
@@ -5,12 +5,11 @@ import { Session } from 'src/entities/session.entity';
 import { SessionService } from './session.service';
 import { AWSService } from '../aws/aws.service';
 import { WinstonModule } from 'nest-winston';
-import { ImageResize } from 'src/helpers/resize-images';
 import { ShopInfo } from 'src/utils/shop-info.util';
 
 @Module({
   imports: [MikroOrmModule.forFeature([Session]), WinstonModule],
   controllers: [SessionController],
-  providers: [SessionService, AWSService, ImageResize, ShopInfo],
+  providers: [SessionService, AWSService, ShopInfo],
 })
 export class SessionModule {}
