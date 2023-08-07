038e4751db5571332c695d2e3a33631edf64d42d
DuyTran <duy.tran@nfq.com>
2023-08-07 15:26:54 +0700

    feat: edit migration file

--- src/database/migrations/Migration20230731232745_change_session_status_name.ts
+++ src/database/migrations/Migration20230731232745_change_session_status_name.ts
@@ -2,10 +2,14 @@ import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20230731232745_change_session_status_name extends Migration {
   async up(): Promise<void> {
-    this.addSql('alter table `session` add `shop_name` text null;');
+    this.addSql(
+      "ALTER TABLE `session` modify `status` enum ('OPEN','LOCKED','PENDINGPAYMENTS','FINISHED') NOT NULL;",
+    );
   }
 
   async down(): Promise<void> {
-    this.addSql('alter table `session` drop `shop_name`;');
+    this.addSql(
+      "ALTER TABLE `session` modify `status` enum ('OPEN','LOCKED','PENDING PAYMENTS','FINISHED') NOT NULL;",
+    );
   }
 }
